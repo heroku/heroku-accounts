4e0b6db96021a51451315499eafdb59a06e2ba2e
Rikki Pitt <rikkipitt@gmail.com>
2016-07-10 11:17:24 +0100

    Add current command to display currenty logged in account

---
+++ commands/current.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const cli = require('heroku-cli-util')
+const co = require('co')
+const accounts = require('../lib/accounts')
+
+function * run (context, heroku) {
+  var account = accounts.current();
+  if (account) {
+    cli.log(`${account}`)
+  }
+}
+
+module.exports = {
+  topic: 'accounts',
+  command: 'current',
+  run: cli.command(co.wrap(run))
+}
