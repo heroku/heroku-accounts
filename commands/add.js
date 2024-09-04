6cd038e276aa31f7bd9fc5a58e031a33672aaaa3
Eric <eblack@salesforce.com>
2024-09-04 14:33:27 -0700

    Address vulnerabilities (#46)
    * Address vulnerabilities
    
    * Replace circleci with workflow
    
    * Switch to require

--- commands/add.js
+++ commands/add.js
@@ -5,20 +5,20 @@ const co = require('co')
 const accounts = require('../lib/accounts')
 
 function * run (context, heroku) {
-  const {name} = context.args
+  const { name } = context.args
   if (accounts.list().find(a => a.name === name)) {
     cli.error(`${name} already exists`)
     cli.exit(1)
   }
 
-  let auth = yield cli.login({sso: context.flags.sso, skipLogout: true})
+  const auth = yield cli.login({ sso: context.flags.sso, skipLogout: true })
   accounts.add(name, auth.email, auth.token)
 }
 
 module.exports = {
   topic: 'accounts',
   command: 'add',
-  args: [{name: 'name'}],
-  flags: [{name: 'sso', description: 'login for enterprise users under SSO'}],
+  args: [{ name: 'name' }],
+  flags: [{ name: 'sso', description: 'login for enterprise users under SSO' }],
   run: cli.command(co.wrap(run))
 }
