072f89d7873172ab1914060728f14b45f0e2dff8
Katy Bowman <katy.bowman@salesforce.com>
2025-06-26 09:45:20 -0400

    test fixes

--- commands/index.js
+++ commands/index.js
@@ -6,7 +6,7 @@ const accounts = require('../lib/accounts')
 
 function * run (context, heroku) {
   cli.warn('This command is now available as part of the Heroku CLI and this plugin has been archived. Please uninstall this plugin by running `heroku plugins:uninstall @heroku-cli/heroku-accounts`.')
-  
+
   for (const account of accounts.list().map(a => a.name)) {
     if (account === accounts.current()) {
       cli.log(`* ${account}`)
